59ac90901b860523da1aa33eca454698c6875935
kucherol <kucherol2108@gmail.com>
2022-11-28 17:22:33 +0200

    create listing form added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,9 +2,11 @@ import {useState} from 'react';
 import {getAuth, updateProfile} from 'firebase/auth';
 import {updateDoc, doc} from 'firebase/firestore';
 import {db} from '../firebase.config';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import {toast} from 'react-toastify';
+import arrowRight from '../assets/svg/keyboardArrowRightnIcon.svg';
+import homeIcon from '../assets/svg/homeIcon.svg';
 
 function Profile() {
     const auth = getAuth();
@@ -88,6 +90,11 @@ function Profile() {
                     />
                 </form>
             </div>
+            <Link to='/create-listing' className='createListing' >
+                <img src={homeIcon} alt="home" />
+                <p>Sell or Rent your home!</p>
+                <img src={arrowRight} alt="arrow" />
+            </Link>
         </main>
     </div>
 }
